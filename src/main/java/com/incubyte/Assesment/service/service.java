edd06109348cf4e30c5405cc817d0d363e1c1106
RAHUL CHANDRAKANT CHAVAN <rahulcchavan.22@gmail.com>
2022-03-18 19:11:27 +0530

    Addition of unknown number of numbers (Refactoring)

--- src/main/java/com/incubyte/Assesment/service/service.java
+++ src/main/java/com/incubyte/Assesment/service/service.java
@@ -10,15 +10,12 @@ public class service {
 		// If length of string is 0 after removing trailing and starting spaces return 0
 		if (numbers.trim().length() == 0)
 			return 0;
-		
+	
 		//Create a string array after split the numbers string according to comma (",")
 		String[] numbersList = numbers.split(",");
 		
-		//If length of string array is 1 then string contains only one element, simply returning numbersList first element after parsing it to Integer 
-		if (numbersList.length == 1)
-			return Integer.parseInt(numbersList[0]);
-		
 		int ans = 0;
+		//Traverse numbersList array and add integer value of each element to the "ans" variable
 		for (String num: numbersList) {
 			ans = ans + Integer.parseInt(num);
 		}
